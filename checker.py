b0777ba0b4211199539726659c292807db5ee1ae
wojtek <hawrylukwojciech1@gmail.com>
2022-10-24 22:02:21 +0200

    Added handling sites from txt files

--- checker.py
+++ checker.py
@@ -1,11 +1,12 @@
-from requests import RequestException
-import requests as req
 import argparse
+import json
+import requests as req
+from requests import RequestException
 
 
 class ConnectionInstance:
     def __init__(self):
-        self.serverstate: str
+        serverstate: str
 
     def test_connection(self, serveraddress: str):
         with req.Session() as currsession:
@@ -17,45 +18,60 @@ class ConnectionInstance:
             except RequestException:
                 self.serverstate = "down"
 
-            print(f"{serveraddress} is {self.serverstate}")
+            #print(f"{serveraddress} is {self.serverstate}")
             return self.serverstate
 
 
-def process_sites(argv):
+def process_site(argv):
     retval = None
-    instancesStatuses = {}
-    for a, siteaddress in argv.items():
-        if siteaddress:
+    instancesstatuses = {}
+    for a, val in argv.items():
+        if val:
             classInstance = ConnectionInstance()
-            instancesStatuses[siteaddress] = classInstance.test_connection(siteaddress)
-            if "down" in instancesStatuses.values():
+            instancesstatuses[val] = classInstance.test_connection(val)
+            if "down" in instancesstatuses.values():
                 retval = "down"
             else:
                 retval = "up"
 
-    print(instancesStatuses)
+    instancesstatuses = json.dumps(instancesstatuses)
+    print(instancesstatuses)
     return retval
+
+
+def process_sites_from_file(servers):
+    instancestatuses = []
+    for server in servers:
+        instancestatus = {}
+        server = server.strip('\n')
+        if len(server) > 3:
+            classInstance = ConnectionInstance()
+            instancestatus[server] = classInstance.test_connection(server)
+            instancestatuses.append(instancestatus)
+
+    instancestatuses = json.dumps(instancestatuses)
+    print(instancestatuses)
+
 
 
 def parse_args():
     my_parser = argparse.ArgumentParser(description="Enter website to check", prefix_chars='-')
-    my_parser.add_argument('site')
+    my_parser.add_argument('-site',
+                           action='store')
     my_parser.add_argument('-two',
                            action='store',
                            help='allows to pass more than one website to check')
+    my_parser.add_argument('-f',
+                           type=argparse.FileType('r'))
     parsedargs = my_parser.parse_args()
     argv = vars(parsedargs)
-    process_sites(argv)
+    if parsedargs.f is not None:
+        txtfile = parsedargs.f
+        lines = txtfile.readlines()
+        process_sites_from_file(lines)
 
-
-def mock_single_argument_parser(firstsite):
-    parsedargs = {'site': firstsite}
-    return process_sites(parsedargs)
-
-
-def mock_multiple_argument_parser(firstsite, secondsite):
-    parsedargs = {'site': firstsite, 'two': secondsite}
-    return process_sites(parsedargs)
+    else:
+        process_site(argv)
 
 
 if __name__ == '__main__':
